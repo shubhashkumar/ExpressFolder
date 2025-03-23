deac84722c8f90e121a2a1bf06432a336145a3b1
Shubhash Kumar <subhashsharmandhruv@gmail.com>
2025-03-23 10:54:32 +0530

    modified technique

--- app.js
+++ app.js
@@ -58,9 +58,9 @@ app.post("/categories", (req, res, next) => {
   res.send(`
      A new categories has been created.`);
 });
-app.use("*", (req, res, next) => {
-  res.status = 404;
-  res.send(`<h1>${res.status} - Page Not Found</h1> `);
+app.get("*", (req, res, next) => {
+ // res.status = 404;
+  res.status(404).send(`<h1> 404 -Page Not Found</h1> `);
 });
 const PORT = 3000;
 app.listen(PORT, () => {
