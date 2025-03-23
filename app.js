4d7008e9b38613142bbb62609a326b3e72fae2cc
Shubhash Kumar <subhashsharmandhruv@gmail.com>
2025-03-23 10:11:37 +0530

    different routes

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express= require('express');
 const app=  express();
 //Create Your Own Middleware and Routing
-//const user="Guest";
+/*
 app.use("/welcome",(req,res,next)=>
 {
    req.user="Guest";
    console.log(req.user);
    res.send(`
     <h1>Welcome,${req.user}</h1>`);
-
 })
+*/
+//Question-Routes
+//1.get orders
+app.get("/orders",(req,res,next)=>
+  {
+     res.send(`
+     Here is the list of all orders.`);
+  })
+//2.post orders
+//1.get orders
+app.post("/orders",(req,res,next)=>
+  {
+     res.send(`
+     A new order has been created.`);
+  })
+  //1.get users
+app.get("/users",(req,res,next)=>
+  {
+     res.send(`
+     Here is the list of all users.`);
+  })
+//2.post users
+//1.get users
+app.post("/users",(req,res,next)=>
+  {
+     res.send(`
+     A new user has been created.`);
+  })
+
 const PORT=3000;
 app.listen(PORT,()=>
 {
