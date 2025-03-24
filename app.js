69af82a1eaa56233cda6ace070f303a447c92b34
Shubhash Kumar <subhashsharmandhruv@gmail.com>
2025-03-24 10:28:16 +0530

    log the method and url

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post("/users", (req, res, next) => {
      A new user has been created.`);
 });
 */
-//Unknown route
+/* Unknown route
 //1.get products
 app.get("/products", (req, res, next) => {
   res.send(`
@@ -62,7 +62,41 @@ app.get("*", (req, res, next) => {
  // res.status = 404;
   res.status(404).send(`<h1> 404 -Page Not Found</h1> `);
 });
-const PORT = 3000;
+*/
+
+//Challenge- Create a Server with Middleware for Logging
+//1.get products
+app.get("/products", (req, res, next) => {
+  console.log(req.method,req.url);
+  res.send(`
+     Here is the list of all products.`);
+});
+//2.post orders
+//1.get orders
+app.post("/products", (req, res, next) => {
+  console.log(req.method,req.url);
+  res.send(`
+     A new products has been added.`);
+});
+//1.get users
+app.get("/categories", (req, res, next) => {
+  console.log(req.method,req.url);
+  res.send(`
+     Here is the list of all categories.`);
+});
+//2.post users
+//1.get users
+app.post("/categories", (req, res, next) => {
+  console.log(req.method,req.url);
+  res.send(`
+     A new categories has been created.`);
+});
+app.get("*", (req, res, next) => {
+ // res.status = 404;
+ console.log(req.method,req.url);
+  res.status(404).send(`<h1> 404 -Page Not Found</h1> `);
+});
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(
     "Server is up and running on port 3000! Ready to handle requests."
